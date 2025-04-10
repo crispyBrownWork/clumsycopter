ff1be69e4610e6a43d8cf19d0f9aa8d5af36c58b
Giovanni Baldovino <gbaldovino1@hwemail.com>
2025-04-10 12:41:29 -0700

    Restructured into folders

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -5,7 +5,7 @@ func _ready():
 	$VBoxContainer/StartButton.grab_focus()
 
 func _on_start_button_pressed():
-	get_tree().change_scene_to_file("res://world_environment.tscn")
+	get_tree().change_scene_to_file("res://scenes/world_environment.tscn")
 
 func _on_quit_button_pressed():
 	get_tree().quit()
